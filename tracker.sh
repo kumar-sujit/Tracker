99c0b3a1b425f32c506e59f8115229f3314aac51
sujitkumar <sujit@addteq.com>
2016-11-23 23:24:01 +0530

    added the script

---
+++ tracker.sh
@@ -0,0 +1,11 @@
+cd /home/addteq/prabhu_test1
+echo "MicroService Name : "$JOB_NAME > tracker.txt
+echo "Branch Name : "$GIT_BRANCH >> tracker.txt
+echo "Build Number : "$BUILD_NUMBER >> tracker.txt
+echo "Commit ID : "$GIT_COMMIT >> tracker.txt
+echo "Date : " `date +"%c"` >> tracker.txt
+git add --all
+git config user.email "sujit@addteq.com"
+git config user.name "sujitkumar"
+git commit -am "$GIT_AUTHOR has made a new change for this micro-service"
+git push origin master
