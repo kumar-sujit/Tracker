41bb6fbd5eb4691801232d127c6199c54ea95aba
sujitkumar <sujit@addteq.com>
2016-12-08 07:38:06 +0530

    latest commit

--- tracker.sh
+++ tracker.sh
@@ -1,12 +1,13 @@
 cd /home/addteq/prabhu_test1
-echo "MicroService Name : "$JOB_NAME > tracker.txt
-echo "Branch Name : "$GIT_BRANCH >> tracker.txt
-echo "Build Number : "$BUILD_NUMBER >> tracker.txt
-echo "Commit ID : "$GIT_COMMIT >> tracker.txt
-echo "Date : " `date +"%c"` >> tracker.txt
-awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' tracker.txt > tracker.json
+echo "MicroService Name" : "$JOB_NAME" > $JOB_NAME.txt
+echo "Branch Name" : "$GIT_BRANCH" >> $JOB_NAME.txt
+echo "Build Number : "$BUILD_NUMBER >> $JOB_NAME.txt
+echo "Commit ID : "$GIT_COMMIT >> $JOB_NAME.txt
+echo "Date : " `date +"%c"` >> $JOB_NAME.txt
+awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' $JOB_NAME.txt > $JOB_NAME.json
 git add --all
 git config user.email "sujit@addteq.com"
 git config user.name "sujitkumar"
 git commit -am "$GIT_AUTHOR has made a new change for this $JOB_NAME"
-git push https://<username>:<password>github.com/kumar-sujit/Tracker.git
+git pull https://kumar-sujit:arankal%4089@github.com/kumar-sujit/Tracker.git master
+git push https://kumar-sujit:arankal%4089@github.com/kumar-sujit/Tracker.git master
