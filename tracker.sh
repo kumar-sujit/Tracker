4f8bbcfa8cf5804e104acc8d2ee43bc5a4189ca9
kumar-sujit <sujit.kumar@addteq.com>
2017-01-03 22:04:16 +0530

    Update tracker.sh
    updated with latest stage script

--- tracker.sh
+++ tracker.sh
@@ -1,22 +1,22 @@
-Git_Tag_Commit=git rev-list --tags --max-count=1
-Git_Latest_Tag=`git describe --tags $Git_Tag_Commit`
-cd /home/addteq/prabhu_test1
-pwd
-echo $JOB_BASE_NAME
-echo "MicroService Name" : "$JOB_BASE_NAME" > $JOB_BASE_NAME.txt
-echo "Branch Name" : "$GIT_BRANCH" >> $JOB_BASE_NAME.txt
-echo "Build Number : "$BUILD_NUMBER >> $JOB_BASE_NAME.txt
-echo "Commit ID : "$GIT_COMMIT >> $JOB_BASE_NAME.txt
-echo "Date : " `date +"%c"` >> $JOB_BASE_NAME.txt
-echo "Build Tag : " $Git_Latest_Tag >> $JOB_BASE_NAME.txt
-echo "Change Author : "$CHANGE_AUTHOR_DISPLAY_NAME >> $JOB_BASE_NAME.txt
-sed -i '/Date/s/:/-/2g' $JOB_BASE_NAME.txt
-awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' $JOB_BASE_NAME.txt > $JOB_BASE_NAME.json
-sed -i '/Date/s/-/:/g' $JOB_BASE_NAME.json
+rm -rf Tracker
+git clone -b Stage --single-branch https://github.com/kumar-sujit/Tracker.git
+cd Tracker
+#Git_Tag_Commit=`git rev-list --tags --max-count=1`
+#Git_Latest_Tag=`git describe --tags $Git_Tag_Commit`
+#echo $Git_Latest_Tag
+#cd ..
+#rm -rf $Microservice
+#echo "Tag after removing directory :"$Git_Latest_Tag
+#cp $MicroServiceName.txt $MicroServiceName-v1.txt
+echo "MicroService Name" : "$MicroServiceName" > $MicroServiceName.txt
+echo "Branch Name" : "$BranchName" >> $MicroServiceName.txt
+echo "Build Number : "$BuildNumber >> $MicroServiceName.txt
+echo "GitTagg      : " $GitTag >> $MicroServiceName.txt
+awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' $MicroServiceName.txt > $MicroServiceName.json
 git add --all
 git config user.email "sujit@addteq.com"
 git config user.name "sujitkumar"
-git commit -am "$GIT_AUTHOR has made a new change for this $JOB_NAME"
-set +x
-git pull https://${credentials}@github.com/kumar-sujit/Tracker.git master
-git push https://${credentials}@github.com/kumar-sujit/Tracker.git master
+git commit -am "$GIT_AUTHOR has made a new change for this $MicroServiceName"
+#set +x
+#git pull https://${credentials}@github.com/kumar-sujit/Tracker.git Dev
+git push https://${credentials}@github.com/kumar-sujit/Tracker.git Stage
