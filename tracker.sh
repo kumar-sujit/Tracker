965d476bd5766174cd44fcd922d3d32f9e420b0e
kumar-sujit <sujit.kumar@addteq.com>
2017-01-17 21:00:03 +0530

    Update tracker.sh
    jsons based on pom version

--- tracker.sh
+++ tracker.sh
@@ -3,16 +3,16 @@ Git_Latest_Tag=`git describe --tags $Git_Tag_Commit`
 cd /home/addteq/prabhu_test1
 pwd
 echo $JOB_BASE_NAME
-echo "MicroService Name" : "$JOB_BASE_NAME" > $JOB_BASE_NAME.txt
-echo "Branch Name" : "$GIT_BRANCH" >> $JOB_BASE_NAME.txt
-echo "Build Number : "$BUILD_NUMBER >> $JOB_BASE_NAME.txt
-echo "Commit ID : "$GIT_COMMIT >> $JOB_BASE_NAME.txt
-echo "Date : " `date +"%c"` >> $JOB_BASE_NAME.txt
-echo "Build Tag : " $Git_Latest_Tag >> $JOB_BASE_NAME.txt
-echo "Change Author : "$CHANGE_AUTHOR_DISPLAY_NAME >> $JOB_BASE_NAME.txt
-sed -i '/Date/s/:/-/2g' $JOB_BASE_NAME.txt
-awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' $JOB_BASE_NAME.txt > $JOB_BASE_NAME.json
-sed -i '/Date/s/-/:/g' $JOB_BASE_NAME.json
+echo "MicroService Name" : "$JOB_BASE_NAME" > $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Branch Name" : "$GIT_BRANCH" >> $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Build Number : "$BUILD_NUMBER >> $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Commit ID : "$GIT_COMMIT >> $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Date : " `date +"%c"` >> $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Build Tag : " $Git_Latest_Tag >> $JOB_BASE_NAME-$POM_VERSION.txt
+echo "Change Author : "$CHANGE_AUTHOR_DISPLAY_NAME >> $JOB_BASE_NAME-$POM_VERSION.txt
+sed -i '/Date/s/:/-/2g' $JOB_BASE_NAME-$POM_VERSION.txt
+awk -F: 'BEGIN {print "{" }{print "\"" $1"\":" " \""$2"\","}END{print "}"}' $JOB_BASE_NAME-$POM_VERSION.txt > $JOB_BASE_NAME-$POM_VERSION.json
+sed -i '/Date/s/-/:/g' $JOB_BASE_NAME-$POM_VERSION.json
 git add --all
 git config user.email "sujit@addteq.com"
 git config user.name "sujitkumar"
